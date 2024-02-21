181c2df0fc0f59876edac8692d8f2535dc03c309
MortalityWatch <mortalitywatch@proton.me>
2024-02-21 10:32:59 +0100

    restore NA

--- covid19/nzl/foia.r
+++ covid19/nzl/foia.r
@@ -1,6 +1,16 @@
 source("./lib/common.r")
 options(warn = 1)
 
+# Monthly totals
+all_deaths <- read.csv("./data_static/VSD349801_20240217_112550_45.csv") |>
+  as_tibble() |>
+  setNames(c("date", "deaths")) |>
+  mutate(
+    date = make_yearmonth(year = left(date, 4), month = right(date, 2)),
+    deaths = as.integer(deaths)
+  ) |>
+  filter(!is.na(date), date >= make_yearmonth(year = 2010, month = 1))
+
 # DL FOIA data
 url <- paste0(
   "https://fyi.org.nz/request/25021/response/96520/attach/5/",
@@ -15,7 +25,6 @@ order <- c("0-20", "21-40", "41-60", "61-80", "81-99", "100+")
 df <- data |>
   setNames(c("date", "age_group", "doses", "days_since_last", "deaths")) |>
   mutate(
-    date = make_yearmonth(year = left(date, 4), month = right(date, 2)),
     age_group = case_when(
       age_group == "0 to 20" ~ "0-20",
       age_group == "21 to 40" ~ "21-40",
@@ -27,20 +36,63 @@ df <- data |>
     ),
     doses = as.integer(doses),
     deaths = as.integer(deaths),
-    vaccinated = doses > 0
-  ) |>
-  filter(!is.na(date), !is.na(age_group))
+    vaccinated = doses > 0,
+    type = case_when(
+      !is.na(deaths) & doses == 0 ~ "unvaccinated",
+      is.na(deaths) & doses == 0 ~ "unvaccinated_suppressed",
+      !is.na(deaths) & doses > 0 ~ "vaccinated",
+      is.na(deaths) & doses > 0 ~ "vaccinated_suppressed"
+    )
+  )
+
+# Calculate suppressed values
+## Use totals to calculate upper bound
+df_totals <- df |> filter(date == "Total")
+# Even few totals are suppressed, use 2 as upper limit here
+df_totals$deaths[is.na(df_totals$deaths)] <- 2
+df <- df |>
+  filter(date != "Total") |>
+  mutate(date = make_yearmonth(year = left(date, 4), month = right(date, 2)))
+
+totals_all <- df_totals |>
+  group_by(vaccinated) |>
+  summarize(deaths = sum(deaths))
+df_all <- df |>
+  group_by(vaccinated) |>
+  summarize(deaths = sum(deaths, na.rm = TRUE))
+na <- totals_all |>
+  inner_join(df_all, by = c("vaccinated")) |>
+  mutate(deaths = deaths.x - deaths.y)
+
+na_vaxx <- na$deaths[na$vaccinated == TRUE]
+na_unvaxx <- na$deaths[na$vaccinated == FALSE]
+n_vaxx <- sum(is.na((df |> filter(vaccinated == TRUE))$deaths))
+n_unvaxx <- sum(is.na((df |> filter(vaccinated == FALSE))$deaths))
+
+df$deaths[is.na(df$deaths) & df$vaccinated] <- na_vaxx / n_vaxx
+df$deaths[is.na(df$deaths) & !df$vaccinated] <- na_unvaxx / n_unvaxx
 
 # Plot
 df2 <- df |>
-  group_by(date, vaccinated) |>
-  summarize(deaths = sum(deaths, na.rm = TRUE)) |>
-  mutate(vaccinated = ifelse(vaccinated, "vaccinated", "unvaccinated"))
+  group_by(date, type) |>
+  summarize(deaths = sum(deaths, na.rm = TRUE), )
+
+fit <- all_deaths |>
+  as_tsibble(index = date) |>
+  filter_index("2010 Jan" ~ "2019 Dec") |>
+  model(TSLM(deaths ~ season() + trend()))
+fc <- forecast(fit, h = "47 months")
 
 chart <- df2 |>
   filter(date <= make_yearmonth(year = 2023, month = 11)) |>
   ggplot(aes(x = date, y = deaths)) +
-  geom_area(position = position_stack(reverse = TRUE), aes(fill = vaccinated)) +
+  geom_area(position = position_stack(reverse = TRUE), aes(fill = type)) +
+  geom_line(
+    data = fc,
+    aes(y = .mean, color = "Trend (2010-2019)"),
+    linetype = "dashed", size = 0.5
+  ) +
+  scale_color_manual(values = "black") +
   labs(
     title = paste0(
       "All-Cause Deaths by COVID-19 Vaccination Status [New Zealand]"
@@ -50,7 +102,7 @@ chart <- df2 |>
     y = "Deaths",
     fill = "COVID-19 vaccinated"
   ) +
-  scale_fill_manual(values = c("#44781d", "#de5075")) +
+  scale_fill_manual(values = c("#44781d", "#90ff3d", "#de5075", "#ff6394")) +
   theme_bw() +
   theme(axis.text.x = element_text(
     angle = 30, hjust = 0.5, vjust = 0.5
@@ -133,8 +185,12 @@ df2 <- df |>
 df2$age_group <- factor(df2$age_group, levels = order)
 chart <- df2 |>
   filter(date <= make_yearmonth(year = 2023, month = 11)) |>
-  ggplot(aes(x = date, y = deaths)) +
-  geom_area(position = position_stack(reverse = TRUE), aes(fill = vaccinated)) +
+  ggplot(aes(x = date)) +
+  geom_area(
+    position = position_stack(reverse = TRUE),
+    aes(y = deaths, fill = vaccinated)
+  ) +
+  # geom_line(aes(y = deaths_ul, color = vaccinated)) +
   labs(
     title = paste0(
       "All-Cause Deaths by COVID-19 Vaccination Status & ",
@@ -167,15 +223,6 @@ df2 <- df |>
   summarize(deaths = sum(deaths, na.rm = TRUE)) |>
   mutate(vaccinated = ifelse(vaccinated, "vaccinated", "unvaccinated"))
 
-all_deaths <- read.csv("./data_static/VSD349801_20240217_112550_45.csv") |>
-  as_tibble() |>
-  setNames(c("date", "deaths")) |>
-  mutate(
-    date = make_yearmonth(year = left(date, 4), month = right(date, 2)),
-    deaths = as.integer(deaths)
-  ) |>
-  filter(!is.na(date), date >= make_yearmonth(year = 2019, month = 1))
-
 chart <- df2 |>
   filter(date <= make_yearmonth(year = 2023, month = 11)) |>
   ggplot(aes(x = date, y = deaths)) +
@@ -205,3 +252,19 @@ chart <- df2 |>
   watermark()
 
 save_chart(chart, "nzl/all-cause-vaxx-status-3", upload = FALSE)
+
+# Plot difference between two monthly datasets
+all_deaths_v <- df |>
+  group_by(date) |>
+  summarize(deaths = sum(deaths, na.rm = TRUE))
+
+all_deaths_v |>
+  inner_join(all_deaths, by = c("date")) |>
+  mutate(diff = deaths.y - deaths.x) |>
+  as_tsibble(index = date) |>
+  autoplot(.var = diff) + labs(
+    title = "Monthly deaths; Difference of total deaths NZL datasets (a) - (b)",
+    subtitle = "(a): infoshare.stats.govt.nz; (b) https://fyi.org.nz/request/25021",
+    x = "",
+    y = ""
+  )
